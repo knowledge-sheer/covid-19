95f92a76dff5b0f92d59a2c3c0c4338b2be259b3
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2020-04-02 22:52:09 +0530

    basic setup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+/* all the routes are listed here */
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  /* add new routes here */
+  { path: "**", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
